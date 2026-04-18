1b2ead4065ecb0f5f5e4b1e9740e58542f1aba52
youssefzsc321-debug <youssefzsc321@gmail.com>
2026-04-18 14:11:27 +0200

    -Add Action Edit

---
+++ Bookify.Web/Core/ViewModel/CreateAndEditCategoryVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookify.Web.Core.ViewModel
+{
+    public class CreateAndEditCategoryVM
+    {
+        public int Id { get; set; }
+        [MaxLength(100,ErrorMessage ="Max Length Is 100 Charcter")]
+        public string Name { get; set; } = null!;
+    }
+}
