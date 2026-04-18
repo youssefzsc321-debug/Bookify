651ea7ae48869e47d7c913c8d4047eaa5ccc4cae
youssefzsc321-debug <youssefzsc321@gmail.com>
2026-04-18 14:12:03 +0200

    -Add Action Edit on Category Controller

--- Bookify.Web/Controllers/CategoriesController.cs
+++ Bookify.Web/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿
+using Bookify.Web.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bookify.Web.Controllers
@@ -12,10 +13,63 @@ namespace Bookify.Web.Controllers
             _context = context;
         }
 
+       
         public IActionResult Index()
         {
+            
             var categories = _context.Categories.ToList();
             return View(categories);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("CreateAndEdit");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CreateAndEditCategoryVM model)
+        {
+            if (!ModelState.IsValid)
+                return View("CreateAndEdit", model);
+
+            var category = new Category { Name = model.Name };
+            _context.Add(category);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var cat=_context.Categories.FirstOrDefault(c => c.Id == id);
+            if (cat == null)
+                return NotFound();
+
+            var category=new CreateAndEditCategoryVM { Name = cat.Name ,Id=id};
+            return View("CreateAndEdit", category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CreateAndEditCategoryVM model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("CreateAndEdit", model);
+            }
+            var cat=_context.Categories.FirstOrDefault(x=>x.Id== model.Id);
+            if (cat == null) return NotFound();
+            cat.Name= model.Name;
+            cat.LastUpdatedOn= DateTime.Now;    
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
     }
+
 }
+
--- Bookify.Web/GlobalUsings.cs
+++ Bookify.Web/GlobalUsings.cs
@@ -1,2 +1,3 @@
 ﻿global using System.ComponentModel.DataAnnotations;
 global using Bookify.Web.Data;  
+global using Bookify.Web.Core.ViewModel; 
